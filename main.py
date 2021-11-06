48ac79cf567247d450bdaae083662f0827940a2c
Justas Furmonas <JustasFurmonas@gmail.com>
2021-11-06 12:13:31 +0200

    Added event stream change to tensor library

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ import pandas as pd
 from option_parser import PrepareArguments
 from event_parser import event_info
 from visualize_events import viz_events
+from event_reader.event_readers import FixedDurationEventReader
+from event_reader.timers import Timer
+from event_reader.inference_utils import events_to_voxel_grid_pytorch, get_device
 
-def main():
-    parser = PrepareArguments()
-    args = parser.parse_args()
+def main(args):
     fileName = args.input_file
     size = tuple(map(int, args.window_size.split('x')))
     print('Input file -', fileName)
@@ -18,19 +19,24 @@ def main():
     return fileName, size
 
 if __name__ == "__main__":
-    fileName, size = main()
+    parser = PrepareArguments()
+    args = parser.parse_args()
+    fileName, size = main(args)
     FullFileName = os.path.join(sys.path[0], fileName)
 
+    device = get_device(True)
+
     if os.path.isfile(FullFileName):
-        with open(FullFileName, 'r') as file:
-            print('Starting visualize process, press q to exit')
-            header = pd.read_csv(FullFileName, index_col=False, nrows=0).columns.tolist()
-            events = event_info()
-            events.check_header(header)
-            print('Header content: {}'.format(events.event_header))
-            if os.path.isfile(FullFileName):
-                event_visualizer = viz_events(size)
-                event_visualizer.start(FullFileName)
+        event_window_iterator = FixedDurationEventReader(FullFileName)
+        print('Starting visualize process, press q to exit')
+        with Timer('Processing entire dataset'):
+            for event_window in event_window_iterator:
+                last_timestamp = event_window[-1, 0]
+                with Timer('Building event tensor'):
+                    event_tensor = events_to_voxel_grid_pytorch(event_window, 5, 240, 180, device)
+                
+                num_events_in_window = event_window.shape[0]
+
     else:
         print('File does not exist -', FullFileName)
 
--- option_parser.py
+++ option_parser.py
@@ -5,5 +5,6 @@ def PrepareArguments():
     parser.add_argument('-i', '--input_file', required=True, type=str,
                         help='Define CSV INPUT_FILE, where raw event data is to be found')
     parser.add_argument('-s', '--window_size', default='320x240', type=str,
-                        help='Define WINDOW_SIZE of output window')
+                        help='Define WINDOW_SIZE of output window, default - 320x240')
+
     return parser
